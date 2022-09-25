0ac4459fc267777767b526c38fdd16f0866c6207
AmitGithub2006 <105305969+AmitGithub2006@users.noreply.github.com>
2022-09-25 13:42:21 +0530

    todo

--- amit1/src/components/TodoApp.jsx
+++ amit1/src/components/TodoApp.jsx
@@ -1,14 +1,43 @@
 import React, { useState } from "react";
 
 export const TodoApp = () => {
+  const [todo, setTodo] = useState([]);
+  const [items, setItems] = useState("");
 
-    const [todo, setTodo] = useState([])
-    const [items, setItems] = useState("")
+  const addTodo = () => {
+    setTodo([...todo, items]);
+    setItems("");
+  };
 
-    return (
-        <>
-        <input type="text" value={items} placeholder="Write your todo here..." onChange={(e) => setItems(e.target.value)} />
-        <button>Add Todo</button>
-        </>
-    )
-}
+  return (
+    <>
+      <h1 className="heading">
+        <i class="fa-solid fa-list"></i>Todo App
+      </h1>
+      <div className="container">
+        <input
+          type="text"
+          id="input"
+          value={items}
+          placeholder={"write your todo here..."}
+          onChange={(e) => setItems(e.target.value)}
+        />
+        <button onClick={addTodo}>add todo</button>
+        {todo.map((item) => (
+          <div>
+            <span>{item}</span>
+            <button>Edit</button>
+            <button
+              onClick={() => {
+                let filteredtodo = todo.filter((value) => value !== item);
+                setTodo([...filteredtodo]);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
